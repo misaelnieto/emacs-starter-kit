cdc26610130d96a0d08d5f7ba0fed0a456e1f73d
tzicatl <tzicatl@gmail.com>
2010-09-24 13:49:53 -0500

    Hacer que la ventana de emacs se maximice al arrancar

--- tzicatl.el
+++ tzicatl.el
@@ -1,3 +1,6 @@
+;;Primero quiero que emacs se haga fullscreen. Siempre
+(toggle-fullscreen)
+
 (require 'color-theme-tangotango "temas/color-theme-tangotango.el")
 (color-theme-tangotango)
 (set-face-attribute 'default nil :height 100)
